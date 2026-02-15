5f23a8de5cc8300db71ed1b288dd9af18f334fcc
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-02-15 04:26:50 +0100

    hbtehe

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
+      "http://localhost:5000",
       "https://gestechbackend-production.up.railway.app",
     ],
     credentials: true,
